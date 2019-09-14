e7ab091718ea963586bafbbc0b5586ef448f5263
mario <air.hammer@gmail.com>
2019-09-14 02:37:16 -0300

    Crud anidado con asociaciòn

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,23 @@
+class EmployeesController < ApplicationController
+
+  def create
+    @employee = Employee.create(employee_params)
+    @employee.company = Company.find(params[:company_id])
+    @employee.save
+
+    redirect_to @employee.company, notice: "Empleado creado satisfactoriamente"
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+
+    redirect_to @employee.company, notice: "Empleado eliminado satisfactoriamente"
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :area_id)
+  end
+
+end
